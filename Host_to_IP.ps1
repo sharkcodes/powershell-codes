a58ae690a7c3e8541910abcae42e9df9b06162bf
sharkcodes <33655692+sharkcodes@users.noreply.github.com>
2017-11-14 16:10:42 +0530

    Add files via upload

---
+++ Host_to_IP.ps1
@@ -0,0 +1,9 @@
+﻿$hostlist = Get-Content .\Desktop\cuba.txt
+$IPs = @()
+foreach ($h in $hostlist)
+{
+$Ip = ((test-connection $h | Select-Object ipv4address -First 1 | select ipv4address).ipv4address).ipaddresstostring
+$IPs += "$Ip  of  $h"
+}
+
+$IPs
